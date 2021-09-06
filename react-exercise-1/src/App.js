be07b287c28933d838c2f8f83fe159929981326c
ultrasoft011 <ultrasoft.011@gmail.com>
2021-09-06 11:37:52 -0500

    refactoring into a single JavaScript object

--- react-exercise-1/src/App.js
+++ react-exercise-1/src/App.js
@@ -18,28 +18,32 @@ const Total = (props) => {
 
 const App = () => {
   // Refactoring into objects
-  const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
   };
 
   return (
     <div>
-      <Header course={course} />
+      <Header course={course.name} />
       {/* The props name will be first and then the name of the variables that I want to show*/}
-      <Content parts={part1.name} exercises={part1.exercises} />
-      <Content parts={part2.name} exercises={part2.exercises} />
-      <Content parts={part3.name} exercises={part3.exercises} />
-      <Total exercises={`Number of exercises ${part1.exercises + part2.exercises + part3.exercises}`} />
+      <Content parts={course.parts[0].name} exercises={course.parts[0].exercises} />
+      <Content parts={course.parts[1].name} exercises={course.parts[1].exercises} />
+      <Content parts={course.parts[2].name} exercises={course.parts[2].exercises} />
+      <Total exercises={`This course has ${course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} exercises`} />
     </div>
   );
 };
