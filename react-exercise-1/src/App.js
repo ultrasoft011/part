8808639d92cad74af8c61ef9ef718239a2d343a1
ultrasoft011 <ultrasoft.011@gmail.com>
2021-09-06 11:17:21 -0500

    refactoring into objects exercise 1.3

--- react-exercise-1/src/App.js
+++ react-exercise-1/src/App.js
@@ -5,30 +5,41 @@ const Header = (props) => {
 };
 
 const Content = (props) => {
-  return <p>{props.parts} {props.exercises}</p>
-}
+  return (
+    <p>
+      {props.parts} {props.exercises}
+    </p>
+  );
+};
 
 const Total = (props) => {
-  return <p>{props.exercises}</p>
-}
+  return <p>{props.exercises}</p>;
+};
 
 const App = () => {
+  // Refactoring into objects
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
 
   return (
     <div>
       <Header course={course} />
       {/* The props name will be first and then the name of the variables that I want to show*/}
-      <Content parts={part1} exercises={exercises1} />
-      <Content parts={part2} exercises={exercises2} />
-      <Content parts={part3} exercises={exercises3} />
-      <Total exercises={`Number of exercises ${exercises1 + exercises2 + exercises3}`} />
+      <Content parts={part1.name} exercises={part1.exercises} />
+      <Content parts={part2.name} exercises={part2.exercises} />
+      <Content parts={part3.name} exercises={part3.exercises} />
+      <Total exercises={`Number of exercises ${part1.exercises + part2.exercises + part3.exercises}`} />
     </div>
   );
 };
