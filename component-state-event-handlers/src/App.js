68d369b74a55a3bcb6eec53b693c681ba760fd46
ultrasoft011 <ultrasoft.011@gmail.com>
2021-09-07 15:16:26 -0500

    destructuring

--- component-state-event-handlers/src/App.js
+++ component-state-event-handlers/src/App.js
@@ -1,16 +1,28 @@
 import "./App.css";
 
 // Hello component
+
+// I can apply destructuring directly passed to the component * const Hello = ({name, age}) => { *
 const Hello = (props) => {
+  // Since props is an object we can streamline our component by assigning the value of the properties directly into variables
+  // const name = props.name;
+  // const age = props.age;
+
+  // Destructuring the variables: extract and gather the values of an object's properties into separate variables.
+  const {name, age} = props;
+
   // Guess the year of birth of the person - helper function "bornYear"
-  const bornYear = () => {
-    const yearNow = new Date().getFullYear();
-    return yearNow - props.age;
-  };
+  // const bornYear = () => {
+  //   const yearNow = new Date().getFullYear();
+  //   return yearNow - age;
+  // };
+
+  // bornYear function refactored
+  const bornYear = () => new Date().getFullYear() - age;
   return (
     <div>
       <p>
-        Hello {props.name}, you are {props.age} years old
+        Hello {name}, you are {age} years old
       </p>
       <p>You probably born on {bornYear()}</p>
     </div>
